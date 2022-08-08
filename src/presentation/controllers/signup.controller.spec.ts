702061cb53fc7c09f3998bada5fa143444b97ed6
EmanuelVogt <contatoemanuel3@gmail.com>
2022-08-08 15:28:55 -0300

    test: ensure SignUpcontroller returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -0,0 +1,15 @@
+import { SignUpController } from "./signup.controller";
+
+describe("signup controller", () => {
+  test("should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        email: "any_email",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
