26b0f092eb48fbab7b4e3a4030d6ebbb4fd3bd3f
EmanuelVogt <contatoemanuel3@gmail.com>
2022-08-08 16:32:40 -0300

    refactor: ensure SignupController.handle is assigned by https interfaces

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
 export class SignUpController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
     const { name, email } = httpRequest.body;
     if (!name) {
       return {
