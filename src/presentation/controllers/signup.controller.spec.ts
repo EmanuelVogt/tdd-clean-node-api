3064828e85ec41d371963f46c0f63b8b03a84a0f
EmanuelVogt <contatoemanuel3@gmail.com>
2022-08-08 18:06:24 -0300

    test: ensure return 400 status code and error masage if no password and passwordConfirmation as provided

--- src/presentation/controllers/signup.controller.spec.ts
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -12,7 +12,7 @@ describe("signup controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('name'));
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
   test("should return 400 and an error message if no email is provided", () => {
     const sut = new SignUpController();
@@ -20,10 +20,39 @@ describe("signup controller", () => {
       body: {
         name: "any_name",
         password: "any_password",
+        confirmationPassword: "any_confirm_password",
       },
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+  test("should return 400 and an error message if no password is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        passwordConfirmation: "any_confirm_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
+  test("should return 400 and an error message if no passwordConfirmation is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
   });
 });
