e7b4da863bc1dd96be68dfbb38845a70a71f9828
EmanuelVogt <contatoemanuel3@gmail.com>
2022-08-10 22:27:45 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/crypt/bcrypt-adapter.spec.ts
+++ src/infra/crypt/bcrypt-adapter.spec.ts
@@ -12,6 +12,12 @@ const makeSut = (): SutTypes => {
     sut,
   };
 };
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("any_hash"));
+  },
+}));
 describe("Bcrypt adapter", () => {
   test("should calls Bcrypt with correct value", async () => {
     const { sut } = makeSut();
@@ -19,5 +25,12 @@ describe("Bcrypt adapter", () => {
     await sut.encrypt("any_value");
 
     expect(hashSpy).toHaveBeenCalledWith("any_value", 12);
+  });
+
+  test("should return a hash on success", async () => {
+    const { sut } = makeSut();
+    const hash = await sut.encrypt("any_value");
+
+    expect(hash).toBe("any_hash");
   });
 });
