b48e9823090d1f37940f4f92484ca7e2540c1bb9
EmanuelVogt <contatoemanuel3@gmail.com>
2022-08-10 21:46:38 -0300

    test: ensure DbAccount return an new account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -103,4 +103,20 @@ describe("DbAddAccount usecase", () => {
     const promise = sut.create(accountData);
     expect(promise).rejects.toThrow();
   });
+
+  test("should return an account on success", async () => {
+    const { sut } = makeSut();
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password",
+    };
+    const account = await sut.create(accountData);
+    expect(account).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password",
+    });
+  });
 });
