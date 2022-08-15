aed193b4cf8da01c98c14a0f8bb12efb865f3e4c
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-15 00:03:02 -0300

    refactor: add aditional config to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,7 +3,9 @@ export const MongoHelper = {
   client: null as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri, {});
+    this.client = await MongoClient.connect(uri, {
+      useUnifiedTopology: true
+    });
   },
 
   async disconect(): Promise<void> {
