07c496ec9d571508648724d8cc8c231888b17669
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-17 19:02:48 -0300

    feat: ensure return 400 if no email is provide

--- src/presentation/controllers/login/login.controlller.ts
+++ src/presentation/controllers/login/login.controlller.ts
@@ -1,7 +1,26 @@
+import { MissingParamError } from "../../errors";
+import { badRequest, serverError } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols"
 
 export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise(resolve => resolve(null))
+    try {
+      const requiredFields = [
+        "email",
+        "password",
+      ];
+
+      const { email, password } = httpRequest.body;
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+
+      return new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
