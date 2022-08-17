013b79e471db84f0725a1b2df3d4586916582469
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-17 19:03:26 -0300

    test: ensure LoginController return 400 if no email is provided

---
+++ src/presentation/controllers/login/login.controller.spec.ts
@@ -0,0 +1,54 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http-helper";
+import { HttpRequest } from "../../protocols";
+import { EmailValidator } from "../../protocols";
+import { LoginController } from "./login.controlller";
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    ensureIsValid(): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+interface SutTypes {
+  emailValidatorStub: EmailValidator;
+  fakeError: Error,
+  httpRequest: HttpRequest
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController()
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  const httpRequest = {
+    body: {
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+      passwordConfirmation: "any_password",
+    },
+  };
+  return {
+    emailValidatorStub,
+    fakeError,
+    httpRequest,
+    sut
+  };
+};
+describe('login controller', () => {
+  test('should return MissingParamError with 400 status when no email is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        password: "any_password",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
+  });
+});
