9fe4d45ae0e9aa4fce344f38926811ddcf93fee9
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-18 15:38:59 -0300

    test: ensure return correctly errors in ValidationComposite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,25 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-fields-validation';
+import { Validation } from '../../presentation/helpers/validators/validation';
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
+import { makeSignupValidationFactory } from './signup-validation'
+
+jest.mock("../../presentation/helpers/validators/validation-composite")
+
+describe('SignupValidation Factory', () => {
+  test('should call ValidationComposite with all validation fields', () => {
+    makeSignupValidationFactory()
+    const validations: Validation[] = []
+
+    const requiredFields = [
+      "email",
+      "name",
+      "password",
+      "passwordConfirmation"
+    ]
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  });
+});
