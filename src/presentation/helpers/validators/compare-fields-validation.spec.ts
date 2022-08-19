807f2de3dbaf3dc3b01a2e334b1081caa6ef45b8
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-18 23:33:53 -0300

    test: ensure CompareFieldsValidation work correctly

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError, MissingParamError } from "../../errors";
+import { CompareFieldsValidation } from "./compare-fields-validation";
+
+interface SutTypes {
+  sut: CompareFieldsValidation
+}
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldsValidation('field', 'field_two')
+
+  return {
+    sut
+  }
+}
+describe('CompareFieldsValidation', () => {
+  test('should return InvalidParamError if validation fails', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value', field_two: 'any_value_two' })
+    expect(error).toEqual(new InvalidParamError('field_two'))
+  })
+
+  test('should not return MissingParamError if validation success', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'field', field_two: 'field' })
+    expect(error).toBeFalsy()
+  })
+});
