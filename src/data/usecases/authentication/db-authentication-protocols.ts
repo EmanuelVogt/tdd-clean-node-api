0f735477e5bd17c14f0686061a4829ff7e46cd82
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-19 17:14:29 -0300

    feat: ensure return null if HashCompare returns null

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,1 +1,2 @@
 export * from '../../protocols/db/load-account-by-email-repository'
+export * from '../../protocols/criptography/hash-comparer'
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,21 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from './db-authentication-protocols'
+import { HashComparer, LoadAccountByEmailRepository } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
-  constructor(private readonly loacAccountByEmailRepository: LoadAccountByEmailRepository) { }
+  constructor(
+    private readonly loacAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) { }
+
   async auth({ email, password }: AuthenticationModel): Promise<string> {
     const account = await this.loacAccountByEmailRepository.load(email)
-
     if (!account) {
       return null
     }
-
+    const passwordMatch = this.hashComparer.compare(password, account.password)
+    if (!passwordMatch) {
+      return null
+    }
     return new Promise(resolve => resolve('any_token'))
   }
 }
