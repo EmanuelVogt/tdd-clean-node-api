1afdf98efa3a9e48e480eef02d6df956d7967bbf
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-18 23:00:28 -0300

    fix: should return httpResponse correctly

--- src/main/adapter/express-route-adapter.ts
+++ src/main/adapter/express-route-adapter.ts
@@ -7,11 +7,12 @@ export const adapterRoute = (controller: Controller) => {
 			body: req.body
 		}
 		const httpResponse = await controller.handle(httpRequest)
-		if (httpResponse.statusCode === 200) {
+		if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
 			res.status(httpResponse.statusCode).json(httpResponse.body)
-		}
-		if (httpResponse.statusCode === 500) {
-			res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+		} else {
+			res.status(httpResponse.statusCode).json({
+				error: httpResponse.body.message
+			})
 		}
 	}
 }
