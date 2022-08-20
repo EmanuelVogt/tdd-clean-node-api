4ec63ab31c6c533f615f5f36b45f859ac83afdcf
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-20 12:49:51 -0300

    test: ensure BcryptAdapter calls bcrypt compare with correct values

--- src/infra/crypt/bcrypt-adapter.spec.ts
+++ src/infra/crypt/bcrypt-adapter.spec.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
 interface SutTypes {
@@ -17,9 +17,13 @@ jest.mock("bcrypt", () => ({
   async hash(): Promise<string> {
     return new Promise((resolve) => resolve("any_hash"));
   },
+  async compare(): Promise<boolean> {
+    return new Promise((resolve) => resolve(true));
+  },
 }));
+
 describe("Bcrypt adapter", () => {
-  test("should calls Bcrypt with correct value", async () => {
+  test("should call hash with correct value", async () => {
     const { sut } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.hash("any_value");
@@ -27,14 +31,14 @@ describe("Bcrypt adapter", () => {
     expect(hashSpy).toHaveBeenCalledWith("any_value", 12);
   });
 
-  test("should return a hash on success", async () => {
+  test("should return a valid hash on hash success", async () => {
     const { sut } = makeSut();
     const hash = await sut.hash("any_value");
 
     expect(hash).toBe("any_hash");
   });
 
-  test("should throw if Bcrypt throws", async () => {
+  test("should throw if hash throws", async () => {
     const { sut } = makeSut();
     const hashSpy = jest.spyOn(bcrypt, "hash") as unknown as jest.Mock<
       ReturnType<(key: string) => Promise<string>>,
@@ -43,9 +47,15 @@ describe("Bcrypt adapter", () => {
     hashSpy.mockReturnValueOnce(
       new Promise((resolve, reject) => reject(new Error()))
     );
-
     const promise = sut.hash("any_value");
-
     await expect(promise).rejects.toThrow();
+  });
+
+  test("should call compare with correct values", async () => {
+    const { sut } = makeSut();
+    const hashSpy = jest.spyOn(bcrypt, "compare");
+    await sut.compare("any_value", "any_hash");
+
+    expect(hashSpy).toHaveBeenCalledWith("any_value", "any_hash");
   });
 });
