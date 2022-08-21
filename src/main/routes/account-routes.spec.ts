68d875eb3e80886ecb23f1d5cd8c86e034422581
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-21 18:53:13 -0300

    test: ensure returns 200 on login

--- src/main/routes/account-routes.spec.ts
+++ src/main/routes/account-routes.spec.ts
@@ -1,6 +1,9 @@
+import { Collection } from "mongodb";
 import request from "supertest";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
+import bcrypt from 'bcrypt'
+let accountCollection: Collection
 
 describe("Account routes", () => {
   beforeAll(async () => {
@@ -12,7 +15,7 @@ describe("Account routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -28,6 +31,22 @@ describe("Account routes", () => {
         })
         .expect(200);
     });
-
+  });
+  describe('POST /login', () => {
+    test("should return 200 on login", async () => {
+      const password = await bcrypt.hash('any_password', 12)
+      await accountCollection.insertOne({
+        name: "any_name",
+        email: "any_email@mail.com",
+        password
+      })
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "any_email@mail.com",
+          password: "any_password",
+        })
+        .expect(200);
+    });
   });
 });
