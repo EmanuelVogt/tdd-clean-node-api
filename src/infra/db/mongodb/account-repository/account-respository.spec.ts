8d2668a419f6446486cd923849a766ff61b635fa
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-21 15:12:25 -0300

    test: ensure AccountMongoRepository returns an account on loadAccountByEmail success

--- src/infra/db/mongodb/account-repository/account-respository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-respository.spec.ts
@@ -1,6 +1,26 @@
+import { Collection } from "mongodb";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account-repository";
 
+let accountCollection: Collection
+
+const fakeAccount =
+{
+  name: "any_name",
+  email: "any_email@mail.com",
+  password: "any_password",
+}
+
+
+interface SutTypes {
+  sut: AccountMongoRepository
+}
+const makeSut = (): SutTypes => {
+  const sut = new AccountMongoRepository()
+  return {
+    sut
+  }
+}
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -11,21 +31,34 @@ describe("Account Mongo Repository", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
-  test("should return an account on success", async () => {
-    const sut = new AccountMongoRepository();
-    const account = await sut.create({
-      name: "any_name",
-      email: "any_email@mail.com",
-      password: "any_password",
-    });
 
+  test("should return an account on create success", async () => {
+    const { sut } = makeSut()
+    const account = await sut.create(fakeAccount);
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe("any_name");
     expect(account.email).toBe("any_email@mail.com");
     expect(account.password).toBe("any_password");
+  });
+
+  test("should return an account on loadAccountByEmail success", async () => {
+    const { sut } = makeSut()
+    await accountCollection.insertOne(fakeAccount)
+    const account = await sut.loadAccountByEmail("any_email@mail.com");
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("any_name");
+    expect(account.email).toBe("any_email@mail.com");
+    expect(account.password).toBe("any_password");
+  });
+
+  test("should return null if loadAccountByEmail fails", async () => {
+    const { sut } = makeSut()
+    const account = await sut.loadAccountByEmail("any_email@mail.com");
+    expect(account).toBeFalsy();
   });
 });
