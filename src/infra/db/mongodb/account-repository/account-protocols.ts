0c500a294df5f99ddd69720ea629c7a61fe3a16c
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-21 15:12:12 -0300

    feat: ensure AccountMongoRepository returns an account on loadAccountByEmail success

--- src/infra/db/mongodb/account-repository/account-protocols.ts
+++ src/infra/db/mongodb/account-repository/account-protocols.ts
@@ -1,3 +1,4 @@
 export * from "../../../../data/protocols/db/add-account-repository";
 export * from "../../../../domain/models/account";
 export * from "../../../../domain/usecases/add-account";
+export * from "../../../../data/protocols/db/load-account-by-email-repository";
--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -2,19 +2,30 @@ import {
   AccountModel,
   AddAccountModel,
   AddAccountRepository,
+  LoadAccountByEmailRepository,
 } from "./account-protocols";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async create(values: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
     const result = await accountCollection.insertOne(values);
-
-    return {
+    return result && {
       email: result.ops[0].email,
       id: result.ops[0]._id,
       name: result.ops[0].name,
       password: result.ops[0].password,
+    };
+  }
+
+  async loadAccountByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const result = await accountCollection.findOne({ email })
+    return result && {
+      email: result.email,
+      id: result._id,
+      name: result.name,
+      password: result.password,
     };
   }
 }
