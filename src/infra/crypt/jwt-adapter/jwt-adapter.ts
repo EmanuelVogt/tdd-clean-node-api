78bd2500977f6074a4838204d2f1920a9c1392fa
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-21 18:52:53 -0300

    fix: fix payload error on jwt sign

--- src/infra/crypt/jwt-adapter/jwt-adapter.ts
+++ src/infra/crypt/jwt-adapter/jwt-adapter.ts
@@ -2,10 +2,12 @@ import jwt from "jsonwebtoken";
 import { Encrypter } from "../../../data/protocols/criptography/encrypter";
 
 export class JwtAdapter implements Encrypter {
-  constructor(private readonly secret: string){}
+  constructor(private readonly secret: string) { }
 
   async encrypt(id: string): Promise<string> {
-    const token = jwt.sign(id, this.secret)
+    const token = jwt.sign({ id }, this.secret, {
+      expiresIn: 10000000000
+    })
     return token
   }
 }
