89fd5c57efdd0dcaf415ea19254e9e9799ae444e
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-21 18:18:06 -0300

    feat: composite LoginControoler in a factory

---
+++ src/main/factories/login-factory/index.ts
@@ -0,0 +1,25 @@
+import { DbAuthentication } from "../../../data/usecases/authentication/db-authentication";
+import { JwtAdapter } from "../../../infra/crypt/jwt-adapter/jwt-adapter";
+import { BcryptAdapter } from "../../../infra/crypt/bcrypt-adapter/bcrypt-adapter";
+import { AccountMongoRepository } from "../../../infra/db/mongodb/account-repository/account-repository";
+import { LogErrorMongoRepository } from "../../../infra/db/mongodb/log-error-repository/log-error-repository";
+import { LoginController } from "../../../presentation/controllers/login/login.controlller";
+import { Controller } from "../../../presentation/protocols";
+import { LogErrorControllers } from "../../decorators/log-error";
+import { makeLoginValidationFactory } from "./login-validation"
+
+export const makeLoginController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter('')
+  const accountMongoRepository = new AccountMongoRepository
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository
+  )
+  const loginController = new LoginController(dbAuthentication, makeLoginValidationFactory())
+  const logErrorRepository = new LogErrorMongoRepository()
+  return new LogErrorControllers(loginController, logErrorRepository)
+}
