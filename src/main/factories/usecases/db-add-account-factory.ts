804042f6c959155e3e3279a088953a66d79532fd
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-08-31 14:35:31 -0300

    feat: implement loadAccountByEmailRepository on DbAddAccount

--- src/main/factories/usecases/db-add-account-factory.ts
+++ src/main/factories/usecases/db-add-account-factory.ts
@@ -6,5 +6,5 @@ export const makeDbAddAccount = (): DbAddAccount => {
   const salt = 12
   const encrypter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(encrypter, accountMongoRepository)
+  return new DbAddAccount(encrypter, accountMongoRepository, accountMongoRepository)
 }
