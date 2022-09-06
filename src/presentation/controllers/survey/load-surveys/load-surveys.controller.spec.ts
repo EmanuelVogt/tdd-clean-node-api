20233df3a61943192b6b559719d8799dd29f425f
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-06 15:14:54 -0300

    test: ensure LoadSurveyController returns 200 if loadSurveys succeeds

--- src/presentation/controllers/survey/load-surveys/load-surveys.controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys.controller.spec.ts
@@ -1,10 +1,45 @@
 import LoadSurveysController from './load-surveys.controller'
 import { LoadSurveys, SurveyModel } from './protocols'
+import MockDate from 'mockdate'
+import { ok } from '../../../helpers/http'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+
+    {
+      id: 'any_id',
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_answer'
+      }],
+      date: new Date()
+    },
+    {
+      id: 'any_id1',
+      question: 'any_question1',
+      answers: [{
+        image: 'any_image1',
+        answer: 'any_answer1'
+      }],
+      date: new Date()
+    },
+    {
+      id: 'any_id2',
+      question: 'any_question2',
+      answers: [{
+        image: 'any_image2',
+        answer: 'any_answer2'
+      }],
+      date: new Date()
+    }
+  ]
+}
 
 const makeLoadSurvey = (): LoadSurveys => {
   class LoadSurveysStub implements LoadSurveys {
-    async load (): Promise<SurveyModel> {
-      return await new Promise(resolve => resolve(null))
+    async load (): Promise<SurveyModel[]> {
+      return await new Promise(resolve => resolve(makeFakeSurveys()))
     }
   }
 
@@ -20,10 +55,24 @@ const makeSut = (): SutTypes => {
   return { sut, loadSurveyStub }
 }
 describe('LoadSurveysController', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should calls LoadSurveys', async () => {
     const { sut, loadSurveyStub } = makeSut()
     const spy = jest.spyOn(loadSurveyStub, 'load')
     await sut.handle({})
     expect(spy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({})
+    expect(response).toEqual(ok(makeFakeSurveys()))
   })
 })
