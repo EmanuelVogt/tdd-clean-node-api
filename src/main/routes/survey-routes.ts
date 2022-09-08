c074b3ea2674be7c059bdccada6c47629c807d3d
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-07 21:40:13 -0300

    feat: add get surveys route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,10 +3,13 @@ import { Router } from 'express'
 import { expressAdapterMiddleware } from '../adapter/express/express-middleware-adapter'
 import { adapterRoute } from '../adapter/express/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/surveys/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controllers/surveys/load-survey-controller-factory'
 import { makeLogErrorControllerFactory } from '../factories/decorators/log-error-controller-decorator-factory'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = expressAdapterMiddleware(makeAuthMiddleware('admin'))
+  const auth = expressAdapterMiddleware(makeAuthMiddleware())
   router.post('/surveys', adminAuth, adapterRoute(makeLogErrorControllerFactory(makeAddSurveyController())))
+  router.get('/surveys', auth, adapterRoute(makeLogErrorControllerFactory(makeLoadSurveysController())))
 }
