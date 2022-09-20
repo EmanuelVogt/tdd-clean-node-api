9ed56408e7621e77ab8a7c76589892c612602168
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-20 06:21:06 -0300

    test: ensure TokenLoginController returns 401 if TokenAuthentication fails

---
+++ src/presentation/controllers/account/token-login/token-login.controller.spec.ts
@@ -0,0 +1,74 @@
+
+import { unautorized } from '@/presentation/helpers/http'
+import {
+  AuthenticatedAccountModel,
+  HttpRequest,
+  TokenAuthentication,
+  TokenAuthenticationModel,
+  Validation
+} from './protocols'
+import { TokenLoginController } from './token-login.controller'
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeTokenAuthentication = (): TokenAuthentication => {
+  class TokenAuthenticationStub implements TokenAuthentication {
+    async auth ({ token }: TokenAuthenticationModel): Promise<AuthenticatedAccountModel> {
+      return await new Promise(resolve => resolve({
+        email: 'any_email',
+        id: 'any_id',
+        name: 'any_name',
+        accessToken: 'any_access_token',
+        role: 'any_role'
+
+      }))
+    }
+  }
+
+  return new TokenAuthenticationStub()
+}
+
+type SutTypes = {
+  fakeError: Error
+  httpRequest: HttpRequest
+  sut: TokenLoginController
+  tokenAuthenticationStub: TokenAuthentication
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const tokenAuthenticationStub = makeTokenAuthentication()
+  const validationStub = makeValidation()
+  const sut = new TokenLoginController(tokenAuthenticationStub, validationStub)
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+
+  const httpRequest = {
+    body: {
+      token: 'any_token'
+    }
+  }
+
+  return {
+    validationStub,
+    fakeError,
+    httpRequest,
+    sut,
+    tokenAuthenticationStub
+  }
+}
+describe('token login controller', () => {
+  test('should return Unautorized with 401 if invalid token is rpovided', async () => {
+    const { sut, httpRequest, tokenAuthenticationStub } = makeSut()
+    jest.spyOn(tokenAuthenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(unautorized())
+  })
+})
