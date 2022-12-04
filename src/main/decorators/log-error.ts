589b901cc2833662ce9e58b61a71efc4036410d5
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-12-03 21:01:14 -0300

    refactor: add types in middlawers and decorators

--- src/main/decorators/log-error.ts
+++ src/main/decorators/log-error.ts
@@ -1,12 +1,12 @@
 import { LogErrorRepository } from '@/data/protocols/db/log-error-repository'
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class LogErrorControllers implements Controller {
   constructor (
     private readonly controller: Controller,
     private readonly logErrorRepository: LogErrorRepository) { }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (httpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
       await this.logErrorRepository.logError(httpResponse.body.stack)
